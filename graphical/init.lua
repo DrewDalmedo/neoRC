f0583add80491365db8712bb28e072452b2793af
Drew Dalmedo <andrewdalmedo@gmail.com>
2026-04-28 09:49:46 -0400

    allow for platform-specific font families and font sizes

--- graphical/init.lua
+++ graphical/init.lua
@@ -1,4 +1,13 @@
-vim.opt.guifont = "UbuntuMono Nerd Font:h22"
+local default_font_size = 22
+
+local font_family = "UbuntuMono Nerd Font"
+local font_size = default_font_size
+
+if vim.fn.has("win32") == 1 then
+    font_size = 14
+end
+
+vim.opt.guifont = font_family .. ":" .. font_size
 
 if vim.g.neovide ~= nil then
     require("neo.graphical.neovide")

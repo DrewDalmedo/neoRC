16c59854add6587eb34f91039a640f438136eb82
Drew Dalmedo <andrewdalmedo@gmail.com>
2026-04-28 09:53:02 -0400

    fix: forgot an h

--- graphical/init.lua
+++ graphical/init.lua
@@ -7,7 +7,7 @@ if vim.fn.has("win32") == 1 then
     font_size = 14
 end
 
-vim.opt.guifont = font_family .. ":" .. font_size
+vim.opt.guifont = font_family .. ":h" .. font_size
 
 if vim.g.neovide ~= nil then
     require("neo.graphical.neovide")
